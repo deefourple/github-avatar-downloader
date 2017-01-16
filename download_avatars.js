4a10d1b5df2e6f09ad056aa1e56b036c797c4868
deefourple <davedehghani@gmail.com>
2017-01-16 23:19:33 +0000

    Logged the information in JSON format to the console

--- download_avatars.js
+++ download_avatars.js
@@ -1,14 +1,31 @@
 var request = require('request');
+var fs = require('fs');
 var GITHUB_USER = "deefourple";
 var GITHUB_TOKEN = "dd92027e4fe310106d942c4f3dc21f9c8885751f";
+
+
 
 console.log('Welcome to the GitHub Avatar Downloader!');
 
 function getRepoContributors(repoOwner, repoName, cb) {
-  var requestURL = 'https://'+ GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
-  console.log(requestURL);
+  var requestURL = ('https://'+ GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors')
+  var options = {
+  url: requestURL,
+  headers: {
+    'User-Agent': 'request'
+  }
+};
+  request.get(options, function(error, response, body) {
+      // deal with a possible error:
+      if (error) {
+        return error;
+      }
+      if (response.statusCode === 200) {
+        var data = JSON.parse(body);
+        console.log(data);
+      }
+    });
 }
-
 getRepoContributors("deefourple", "github-avatar-downloader", function(err, result) {
   console.log("Errors:", err);
   console.log("Result:", result);
