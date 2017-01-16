5dd7709e00cc5cfdd971db270db4f2ba3079562b
deefourple <davedehghani@gmail.com>
2017-01-16 22:20:33 +0000

    requested URL pointing to my github repo

--- download_avatars.js
+++ download_avatars.js
@@ -1,3 +1,15 @@
 var request = require('request');
+var GITHUB_USER = "deefourple";
+var GITHUB_TOKEN = "dd92027e4fe310106d942c4f3dc21f9c8885751f";
 
 console.log('Welcome to the GitHub Avatar Downloader!');
+
+function getRepoContributors(repoOwner, repoName, cb) {
+  var requestURL = 'https://'+ GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
+  console.log(requestURL);
+}
+
+getRepoContributors("deefourple", "github-avatar-downloader", function(err, result) {
+  console.log("Errors:", err);
+  console.log("Result:", result);
+});
