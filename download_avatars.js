79259874ca2e738f294e9ecc1d9a9ab7a3b69e12
deefourple <davedehghani@gmail.com>
2017-02-20 00:49:25 +0000

    Updated variable declaration and functions to ES6

--- download_avatars.js
+++ download_avatars.js
@@ -1,28 +1,30 @@
-var request = require('request');
-var fs = require('fs');
-var GITHUB_USER = "deefourple";
+const request = require('request');
+const fs = require('fs');
+const GITHUB_USER = "deefourple";
 
-var arg = process.argv;
+//Lesson: process.argv is the array of each instance input in Node.js
+let arg = process.argv;
 require('dotenv').config()
 GITHUB_TOKEN: process.env.TOKEN
 
 console.log('Welcome to the GitHub Avatar Downloader!');
 
 //callback function to download all urls found during the for loop
-function downloadImageByURL(url, filePath) {
+downloadImageByURL = (url, filePath) => {
    request.get(url)
   .pipe(fs.createWriteStream(filePath));
 }
 
-function getRepoContributors(repoOwner, repoName, cb) {
+getRepoContributors = (repoOwner, repoName, cb) => {
   //path to user's repository
-  var requestURL = ('https://'+ GITHUB_USER + ':' + process.env.GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors')
-  var options = {
+  let` requestURL = ('https://'+ GITHUB_USER + ':' + process.env.GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors')
+  let` options = {
   url: requestURL,
   headers: {
     'User-Agent': 'request'
   }
-  };
+};
+
   //if the first argument is undefined OR there are more than two arguments { throw error }
   if (arg[2] === undefined || arg.length > 4) {
     throw "Input must be two arguments";
@@ -33,19 +35,19 @@ function getRepoContributors(repoOwner, repoName, cb) {
       }
       if (response.statusCode === 200) {
         //parse our data to JSON
-        var data = JSON.parse(body);
+        let data = JSON.parse(body);
         cb(error, data);
       }
     });
   }
 }
-  //function to iterate through all urls
+  //function to iterate through urls
   getRepoContributors(arg[2], arg[3], function(err,result) {
     if (err) {
       console.log("Errors:", err);
       return err;
     }
-    for (var i = 0; i < result.length; i++ ){
+    for (let i = 0; i < result.length; i++ ){
       downloadImageByURL(result[i].avatar_url, "./avatar/" + result[i].login + ".jpg")
     };
   });
